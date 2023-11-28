09b7c1cabf29a8fff545c19bb7ff1a33347ba051
NetiusSP <fabiomoraisnetosp@hotmail.com>
2023-11-28 16:47:18 +0100

    Fixing router to push to login page if not auth

--- vue3client/src/router/index.js
+++ vue3client/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Login from '../views/Login.vue'
+import {useFhApiStore} from '../stores/FhApiStore'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,16 +9,20 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
+      beforeEnter: isLoggedInn,
       component: HomeView
     },
     {
       path: '/login',
       name: 'login',
+      // beforeEnter: isLoggedInn,
       component: Login
     },
     {
       path: '/about',
       name: 'about',
+      beforeEnter: isLoggedInn,
+
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
@@ -27,3 +32,18 @@ const router = createRouter({
 })
 
 export default router;
+
+async function isLoggedInn (to, from, next) {
+  const fhApiStore = useFhApiStore()
+  await fhApiStore.login().then(res => {
+    if(res.user){
+      next();  
+    } else {
+      next({
+        name: 'login'
+      })
+    }
+    
+  });
+
+}
