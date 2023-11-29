3392a72b0c788719b96e6be1b8c99e23b00b9caa
NetiusSP <fabiomoraisnetosp@hotmail.com>
2023-11-29 16:12:54 +0100

    Wip selected rettigheter

---
+++ vue3client/src/views/User.vue
@@ -0,0 +1,113 @@
+<script setup>
+import { useFhApiStore } from '../stores/FhApiStore';
+import { ref, computed  } from 'vue';
+
+const fhApiStore = useFhApiStore();
+
+let error = ref('');
+let userDetails = ref({});
+let accessToken = ref("");
+let loadingSpinner = ref(false);
+let message = ref({});
+let inputName = ref(fhApiStore.user.name);
+let inputEmail = ref(fhApiStore.user.email);
+let inputPassword = ref("");
+let inputNewPassword = ref("");
+let inputLastName = ref(fhApiStore.user.lastName);
+let selectUserRights = ref(
+	{id: fhApiStore.user.accessLevelId}
+	);
+let selectUserOptions = ref([
+	{ id: 5, name: 'Normal bruker' },
+	{ id: 1, name: 'Administrator' },
+])
+
+
+const isUserAdmin = computed(() => {
+	return fhApiStore.user.accessLevelId == 1 ? true: false; 
+});
+
+</script>
+
+<template>
+	<div class="container">
+		<!-- <AlertTestDatabase /> -->
+		<h2 class="fs-3 fw-bold mt-4 mb-4">Bruker detaljer {{ selectUserRights }}
+			<!-- <Spinner :loadingSpinner="this.loadingSpinner" /> -->
+		</h2>
+
+		<form autocomplete="off" class="row g-3"
+			@submit.prevent="submitUser(inputName, inputLastName, inputEmail, inputPassword, inputNewPassword, selectUserRights)">
+			<div class="col-6">
+				<label for="inputName" class="form-label">Navn</label>
+				<input type="text" :disabled="!isUserAdmin" v-model="inputName" minLength="3" class="form-control" id="inputName"
+					placeholder="eks.John" />
+			</div>
+			<div class="col-6">
+				<label for="inputLastName" class="form-label">Etternavn</label>
+				<input name="inputLastName" autocomplete="new-password" type="text" :disabled="!isUserAdmin"
+					v-model="inputLastName" minLength="3" class="form-control" id="inputLastName" placeholder="eks.Doe" />
+			</div>
+			<div class="col-md-6">
+				<label for="inputEmail" class="form-label">Epost</label>
+				<input type="email" disabled v-model="inputEmail" class="form-control" id="inputEmail"
+					placeholder="eks.john@doe.com" />
+
+			</div>
+			<div class="col-md-6">
+				<label for="inputPassword" class="form-label text-success">Nytt passord</label>
+				<input autocomplete="new-password" type="password" minLength="6" required v-model="inputPassword"
+					class="form-control" id="inputPassword" />
+			</div>
+			<div class="col-md-6">
+				<label for="rettigheter" class="form-label">Rettigheter</label>
+				<!-- <select :disabled="!isUserAdmin" v-model="selectUserRights.id" required class="form-select" aria-label="Rettigheter"
+					name="rettigheter">
+					<option v-for="(selectUserOption, index) in selectUserOptions" :value="selectUserOption" :key="index"
+						:v-if="selectUserOption.id ==  selectUserRights.id">
+						{{ selectUserOption.id }} - {{ selectUserOption.name }}
+					</option>
+				</select> -->
+				
+				<div class="form-check">
+					<input class="form-check-input" :disabled="!isUserAdmin"  type="radio" name="exampleRadios" v-model="selectUserRights" id="exampleRadios1" value="1" :checked="fhApiStore.user.accessLevelId == selectUserRights.id">
+					<label class="form-check-label" for="exampleRadios1">
+						1 - Administrator
+					</label>
+				</div>
+				<div class="form-check">
+					<input class="form-check-input" :disabled="!isUserAdmin"  type="radio" name="exampleRadios" v-model="selectUserRights" id="exampleRadios2" value="5" :checked="fhApiStore.user.accessLevelId == selectUserRights.id">
+					<label class="form-check-label" for="exampleRadios2">
+						5 - Normal bruker
+					</label>
+				</div>
+			</div>
+
+			<div class="col-md-6">
+				<label for="inputPassword" class="form-label">Gjenta passord</label>
+				<input autocomplete="new-password" type="password" minLength="6" required v-model="inputNewPassword"
+					class="form-control" id="inputNewPassword" />
+			</div>
+
+			<div class="col-12">
+				<button type="submit" class="mt-2 btn btn-success">Send
+					<!-- <Spinner :loadingSpinner="loadingSpinner" /> -->
+				</button>
+			</div>
+			<div class="pt-3">
+				<div v-if="message.ok == true" class="alert alert-success alert-dismissible fade show" role="alert">
+					Passord oppdatert. {{ message.statusText }}
+					<button type="button" class="btn-close" @click="closeAlert" aria-label="Close"></button>
+				</div>
+
+				<div v-if="message.ok == false" class="alert alert-danger alert-dismissible fade show" role="alert">
+					Error: {{ message.statusText }}
+					<button type="button" class="btn-close" @click="closeAlert" aria-label="Close"></button>
+				</div>
+			</div>
+		</form>
+	</div>
+</template>
+  
+<style  scoped></style>
+  
