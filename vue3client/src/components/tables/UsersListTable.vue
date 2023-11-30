72967490df5a79d87af2dfa36ad8f743e706e13b
NetiusSP <fabiomoraisnetosp@hotmail.com>
2023-11-30 14:29:37 +0100

    Shows users and wip todo delete and edit

--- vue3client/src/components/tables/UsersListTable.vue
+++ vue3client/src/components/tables/UsersListTable.vue
@@ -2,8 +2,7 @@
 
 import { onMounted, ref } from 'vue';
 import {useFhApiStore} from '../../stores/FhApiStore'
-// import {setTableConfiguration} from "../../utils/dataTablesConfig.js" ;
-
+import {setTableConfiguration} from "../../utils/dataTablesConfig.js" ;
 
 const fhApiStore = useFhApiStore();
 
@@ -15,100 +14,15 @@ const getUsers = async () => {
   })
 }
 
+const deleteUser = async () => {
+  await fhApiStore.deleteUser()
+}
+
 onMounted(() =>{
   getUsers()
-    // .then(() => setTableConfiguration("#usersListTable"));
-
+    .then(() => setTableConfiguration("#usersListTable"));
 })
 
-
-// const apiHost = process.env.API_HOST || "http://" + location.hostname + ":3033";
-// import {setTableConfiguration} from "../../utils/dataTablesConfig.js" ;
-
-// export default {
-//   name: 'UsersListTable',
-//   components: {
-//   },
-//   data(){
-//       return{
-//         showErrorArray: "",
-// 			  allUsersList: [{}],
-//         accessToken: "",
-//       }
-//   },
-//   props: [
-//     "triggerProp"
-//   ],
-//   components:{
-//   },
-
-//   watch:{
-//     allUsersList (newValue , oldValue){
-//       return newValue;
-//     },
-//      triggerProp (newVal, oldVal) {
-//       this.getUsers(this.accessToken);
-//     }
-//   },  
-//   computed:{
-//     userId() {
-//     	let u = this.$store.state.auth.user;
-//       if (u) return u._id;
-//      	return "";
-//     	},
-//   },
-
-//   mounted(){
-// 			this.$store.dispatch("auth/authenticate")
-//         .then(r => {
-//           this.accessToken = r.accessToken;
-//           this.getUsers(this.accessToken).then(
-//             () => setTableConfiguration("#usersListTable")
-//           );
-//         })
-// 			  .catch((error) => {
-// 				  console.error(error);
-// 			});
-//   },
-
-//    methods:{
-//     async getUsers(accessToken) {
-// 			return await fetch(apiHost + "/users/", {
-// 				method: "GET",
-// 				headers: { Authorization: `Bearer ${accessToken}` },
-// 			})
-// 				.then((r) => r.json())
-// 				.then((data) => {this.allUsersList = data.data;})
-// 				.catch((error) => console.error(error));
-// 		},
-
-//     deleteUser(user){  
-//       if (!confirm(`Slett bruker til ${user.name}?`)) return;
-
-//       var myHeaders = new Headers();
-//       myHeaders.append("Authorization", `Bearer ${this.accessToken}`);
-//       var formdata = new FormData();
-
-//       var requestOptions = {
-//         method: 'DELETE',
-//         headers: myHeaders,
-//         body: formdata,
-//         redirect: 'follow',
-//       };
-
-//     fetch( `${apiHost}/users/${user._id}` , requestOptions)
-//       .then(response => {
-//         if(response.ok){this.getUsers(this.accessToken)}; 
-//         console.log(response);
-//         })
-//       .catch(error => console.log('error', error));
-//     },
-
-// 		showError(error) {
-// 			return (this.showErrorArray = error);
-// 		},
-//   },
-// }
 </script>
 
 <template>
@@ -136,7 +50,7 @@ onMounted(() =>{
                     <td>{{ user.email }}</td>
                     <td>{{ user.accessLevelId}}</td>
                     <td class="text-end">
-                        <router-link  title="Rediger" class="btn btn-sm btn-success" :to= "`/userpage/${user._id}`"> 
+                        <router-link  title="Rediger" class="btn btn-sm btn-success" :to= "`/userpage/${user.id}`"> 
                           <i class="fas fa-pen fa-xs"></i>
                         </router-link>          
                     </td>
