e8d4e9c51b9e149a2fdbee9df726a458d95a2500
Rolf-Air <rolf@axicon.no>
2023-12-04 10:11:27 +0100

    Glemte denne

--- vue3client/src/components/__tests__/HelloWorld.spec.js
+++ vue3client/src/components/__tests__/HelloWorld.spec.js
@@ -2,10 +2,31 @@ import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
 import HelloWorld from '../HelloWorld.vue'
+import { setActivePinia, createPinia } from 'pinia'
+import {useCounterStore} from '../../stores/counter'
+
+function factory() {
+  setActivePinia(createPinia())
+  const cntStore = useCounterStore()
+
+  return mount(HelloWorld, { 
+    props: { msg: 'Hello Vitest' },
+    global: {
+      //plugins: [cntStore]
+      provide: {
+        'cntStore': cntStore
+      }
+    } 
+  })
+}
 
 describe('HelloWorld', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
+    const wrapper = factory()
+    console.log(wrapper.wm)
+
+    console.log(wrapper.html)
+    
     expect(wrapper.text()).toContain('Hello Vitest')
   })
 })
